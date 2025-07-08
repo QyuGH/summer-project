6f4b63ac0d33f2d5c512a5375cca725eb79c2cb9
QyuGH <markanton.cahutay@students.isatu.edu.ph>
2025-07-08 22:33:14 +0800

    added interactivity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,57 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark, faX } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-function Header(){
-    return (
-        <header id="header" className="flex justify-between p-4 sticky top-0 bg-secondary z-10">
-            <h1>Anton</h1>
-            <nav className="hidden sm:flex gap-4">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#skills">Skills</a>
-                <a href="#contact">Contact</a>
-            </nav>
-            <div className='block sm:hidden'>
-                <FontAwesomeIcon icon={faBars}/>
-            </div>
-        </header>
-    )
+function Header() {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <>
+      <header
+        id="header"
+        className="flex justify-between p-4 sticky top-0 bg-secondary z-30"
+      >
+        <h1 className="text-2xl">Anton</h1>
+        <nav className="hidden sm:flex gap-4">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </nav>
+        <div
+          className="block w-6 h-6 sm:hidden"
+          onClick={() => setOpen(!isOpen)}
+        >
+          <FontAwesomeIcon icon={isOpen ? faX : faBars} className="text-2xl" />
+        </div>
+      </header>
+
+      <div
+        onClick={() => setOpen(false)}
+        className={`${
+          isOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        } fixed top-0 left-0 h-lvh w-lvw bg-[rgba(0,0,0,0.5)] z-10 transition-opacity duration-500 ease-in-out`}
+      ></div>
+
+      <div
+        id="sidebar"
+        className={`${
+          isOpen ? "right-0" : "-right-full"
+        } sm:hidden fixed top-10 z-20 w-1/2 h-lvh bg-secondary transition-right duration-500 ease`}
+      >
+        <nav className="flex flex-col items-center w-full gap-4 py-4">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#projects">Projects</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Header
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,77 +1,89 @@
-function ProjectCard({title, path, description, tech}){
+function ProjectCard({ title, path, description, tech, link }) {
+  return (
+    <div className="flex flex-col gap-4 border-1 aspect-square w-full max-w-md md:max-w-lg overflow-hidden rounded-2xl">
+      <div className="w-full h-1/2 overflow-hidden">
+        <img
+          src={`${import.meta.env.BASE_URL}${
+            path.startsWith("/") ? path.slice(1) : path
+          }`}
+          alt={title}
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-    return (
-      <div className="flex flex-col gap-4 border-1 aspect-square w-full max-w-md md:max-w-lg overflow-hidden rounded-2xl">
-        <div className="w-full h-1/2 overflow-hidden">
-          <img
-            src={`${import.meta.env.BASE_URL}${
-              path.startsWith("/") ? path.slice(1) : path
-            }`}
-            alt={title}
-            className="w-full h-full object-cover"
-          />
+      <div className="flex flex-col gap-4 px-4">
+        <div>
+          <h1>{title}</h1>
+          <p className="text-xs md:text-sm">{description}</p>
         </div>
 
-        <div className="flex flex-col gap-4 px-4">
-          <div>
-            <h1>{title}</h1>
-            <p className="text-xs md:text-sm">{description}</p>
-          </div>
+        <div className="mt-auto">
+          <p className="text-xs md:text-sm">
+            <span className="font-bold">Tech Stack: </span>
+            {tech}
+          </p>
+        </div>
 
-          <div className="mt-auto">
-            <p className="text-xs md:text-sm">
-              <span className="font-bold">Tech Stack: </span>
-              {tech}
-            </p>
-          </div>
+        <div className="m-auto sm:hidden border-1 p-1 pt-1 rounded">
+          <a href={link} target="_blank" className="mx-auto">
+            View Site
+          </a>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-function Projects(){
+function Projects() {
+  const projects = [
+    {
+      title: "GWA Calculator",
+      path: "/projects/gwa.png",
+      description: "A GWA calculator based on ISATU's grading system.",
+      tech: "HTML, CSS, JavaScript",
+      link: "https://isatu-gwa-calculator.netlify.app/",
+    },
+    {
+      title: "Caffeine Coffee",
+      path: "/projects/caffeine.png",
+      description:
+        "A static coffee shop website I made to practice website animations",
+      tech: "HTML, CSS, JavaScript",
+      link: "https://caffeine-coffee-iloilo.netlify.app/",
+    },
+    {
+      title: "TrabaHunt",
+      path: "/projects/trabahunt.png",
+      description: "A mini job board project I made for our OOP subject.",
+      tech: "HTML, CSS, JavaScript, PHP",
+      link: "https://group-6-web-app.infinityfreeapp.com/TrabaHunt/?i=1",
+    },
+  ];
 
-    const projects = [
-        {
-            title:          "GWA Calculator",
-            path:           "/projects/gwa.png",
-            description:    "A GWA calculator based on ISATU's grading system.",
-            tech:           "HTML, CSS, JavaScript"
-        },
-        {
-            title:          "Caffeine Coffee",
-            path:           "/projects/caffeine.png",
-            description:    "A static coffee shop website I made to practice website animations",
-            tech:           "HTML, CSS, JavaScript"
-        },
-        {
-            title:          "TrabaHunt",
-            path:           "/projects/trabahunt.png",
-            description:    "A mini job board project I made for our OOP subject.",
-            tech:           "HTML, CSS, JavaScript, PHP"
-        }
-    ]
+  return (
+    <section id="projects" className="flex flex-col gap-4">
+      <div>
+        <h1 className="text-4xl sm:text-5xl">PROJECTS</h1>
+        <p>Below are the projects I made throughout my learning journey</p>
+      </div>
 
-    return (
-        <section id="projects" className="flex flex-col gap-4">
-            <div>
-                <h1 className="text-4xl sm:text-5xl">PROJECTS</h1>
-                <p>Below are the projects I made throughout my learning journey</p>
-            </div>
-
-            <div id="project-container" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
-
-                {projects.map(project => (
-                    <ProjectCard key={project.title}
-                    title={project.title} 
-                    path={project.path} 
-                    description={project.description} 
-                    tech={project.tech}/>
-                ))}
-
-            </div>
-        </section>
-    )
+      <div
+        id="project-container"
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center"
+      >
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.title}
+            title={project.title}
+            path={project.path}
+            description={project.description}
+            link={project.link}
+            tech={project.tech}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
